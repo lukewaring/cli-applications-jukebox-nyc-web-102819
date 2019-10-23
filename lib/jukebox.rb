14542f85ade40529346cd5219f0841ebe4b38540
Luke Waring <lukewaring@gmail.com>
2019-10-23 20:00:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,7 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_response = gets.strip
+  user_response = gets.downcase.strip
   
   if (1..9).to_a.include?(user_response.to_i)
     puts "Playing #songs[user_response.to_i - 1]"
