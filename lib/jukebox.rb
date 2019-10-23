8d28716b48c016580d76fb643e359d5b38583bd2
Luke Waring <lukewaring@gmail.com>
2019-10-23 19:48:28 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,8 +24,8 @@ def play(songs)
   
 end
 
-def list(array)
-  array.each_with_index do |item, index|
+def list(songs)
+  songs.each_with_index do |item, index|
     puts "#{index+1}" + ". " + "#{item}"
   end
 end
